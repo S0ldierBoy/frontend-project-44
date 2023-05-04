c2d06f26fe7b68f6973975e579fc1cf1e8386a36
Nick G <nolimit48@yandex.ru>
2023-05-04 22:58:39 +0300

    add.file

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export default function () {
+const greetUser = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
-}
+};
+export default greetUser;
