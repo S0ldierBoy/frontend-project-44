a3a68d8fb295a6d4ee430bf9216c42971f92833c
Nick G <nolimit48@yandex.ru>
2023-05-04 22:54:55 +0300

    add.file

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 import playBrainEven from '../src/games/even-game.js';
 
 playBrainEven();
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 import playPrimeGame from '../src/games/prime-game.js';
 
 playPrimeGame();
