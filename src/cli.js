6963e7dad5eb8dece2f42bfffd821e66b55e9c36
Nick G <nolimit48@yandex.ru>
2023-05-10 18:06:07 +0300

    add.file

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,12 @@
 import readlineSync from 'readline-sync';
 
-const greetUser = () => {
+const greetings = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
 };
 
-export default greetUser;
+export default greetings;
+
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import readlineSync from 'readline-sync';
-import greetUser from './cli.js';
+
+const greetUser = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
 
 const attemptsLimit = 3;
 
