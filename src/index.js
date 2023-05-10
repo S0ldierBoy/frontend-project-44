b709a377a1c3d6d2c03a52f60a5f113892d1fb17
Nick G <nolimit48@yandex.ru>
2023-05-10 18:00:16 +0300

    add.file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import readlineSync from 'readline-sync';
 import greetUser from './cli.js';
 
+const attemptsLimit = 3;
+
 const playGame = (gameDescription, getQuestionAndAnswer) => {
   const name = greetUser();
   console.log(gameDescription);
 
-  for (let i = 0; i < 3; i += 1) {
+  for (let i = 0; i < attemptsLimit; i += 1) {
     const { question, correctAnswer } = getQuestionAndAnswer();
     const userAnswer = readlineSync.question(
       `Question: ${question}\nYour answer: `,
